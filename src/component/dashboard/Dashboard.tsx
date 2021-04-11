843cc8e02f32bb9bd0c4ea9b22f10cd72fc5e4b9
YilunSun <osirisnero@gmail.com>
2021-04-10 21:17:46 -0400

    initialize random pass gen, add antd

--- src/component/dashboard/Dashboard.tsx
+++ src/component/dashboard/Dashboard.tsx
@@ -1,17 +1,27 @@
 import './DashboardStyle.scss';
 import { Link } from 'react-router-dom';
+import { Row, Col } from 'antd';
 
 export default function Dashboard() {
   return (
     <div>
       <h2>Dashboard</h2>
-      <div className='container tool-container'>
-        <div className='row'>
-          <div className='col-sm'><Link to="/tool/random-pass-generator">Random Password Generator</Link></div>
+      <Row>
+        <Col span={6}>
+          <div className='col-sm'>
+            <Link to='/tool/random-pass-generator'>Random Password Generator</Link>
+          </div>
+        </Col>
+        <Col span={6}>
           <div className='col-sm'>One of three columns</div>
+        </Col>
+        <Col span={6}>
           <div className='col-sm'>One of three columns</div>
-        </div>
-      </div>
+        </Col>
+        <Col span={6}>
+          <div className='col-sm'>One of three columns</div>
+        </Col>
+      </Row>
     </div>
   );
 }
--- src/component/tools/RandomPassGen/RandomPassGen.tsx
+++ src/component/tools/RandomPassGen/RandomPassGen.tsx
@@ -1,13 +1,41 @@
 import './RandomPassGenStyle.scss';
+import { Alert, Button, InputNumber } from 'antd';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function RandomPassGen() {
+  const defaultLengthValue = 8;
+
+  function onLengthChange(value: number) {
+    console.log('changed', value);
+    setCount(value);
+  }
+
+  function generatePass() {
+    const result: string[] = [];
+    const characters: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength: number = characters.length;
+    for (let i = 0; i < count; i++) {
+      result.push(characters.charAt(Math.floor(Math.random() * charactersLength)));
+    }
+
+    setResult(result.join(''));
+    setDisplayResult('flex');
+  }
+
+  const [count, setCount] = useState(defaultLengthValue);
+  const [displayResult, setDisplayResult] = useState('none');
+  const [result, setResult] = useState('');
   return (
     <div>
       <div className='header'>
         <h2 className='title'>RandomPassGen</h2>
+        <Link to='/dashboard'>Home</Link>
       </div>
       <div className='content'>
-        <input type='password' className='form-control' id='inputPassword'></input>
+        <InputNumber min={4} max={100} defaultValue={defaultLengthValue} onChange={onLengthChange} />{' '}
+        <Button type='primary' onClick={generatePass}>{`Generate ${count}-letter password`}</Button>
+        <Alert style={{ display: displayResult }} message={result} type='success' showIcon />
       </div>
       <div className='footer'></div>
     </div>
